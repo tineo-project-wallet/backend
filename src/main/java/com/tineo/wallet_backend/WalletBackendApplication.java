a74891079280f9186901dce0ef263a18501d01ce
tineojf <92743762+tineodev@users.noreply.github.com>
2024-12-10 14:55:15 -0500

    Initial files springboot

---
+++ src/main/java/com/tineo/wallet_backend/WalletBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tineo.wallet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tineo/wallet_backend/WalletBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tineo.wallet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
