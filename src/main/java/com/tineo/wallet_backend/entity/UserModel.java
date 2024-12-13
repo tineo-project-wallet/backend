5b7c5f2a4bb49a12c8b9014f38c29e06e2607221
tineojf <92743762+tineodev@users.noreply.github.com>
2024-12-13 14:01:11 -0500

    feat: add usermodel

---
+++ src/main/java/com/tineo/wallet_backend/entity/UserModel.java
@@ -0,0 +1,29 @@
+package com.tineo.wallet_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "app_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
