329605b1d777576bbf0cce071be276ed8eff6f5a
tineojf <92743762+tineodev@users.noreply.github.com>
2024-12-14 15:00:26 -0500

    feat: add user repository

---
+++ src/main/java/com/tineo/wallet_backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tineo.wallet_backend.repository;
+
+import com.tineo.wallet_backend.entity.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
