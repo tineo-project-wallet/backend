4e1785ee42413c46e19a6aa8c24cbc3aa92a6dca
tineojf <92743762+tineodev@users.noreply.github.com>
2025-01-10 12:12:12 -0500

    fix: set protected endpoints with roles

--- src/main/java/com/tineo/wallet_backend/security/SecurityConfig.java
+++ src/main/java/com/tineo/wallet_backend/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.tineo.wallet_backend.config.Constant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,11 +32,18 @@ public class SecurityConfig {
                 .headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(routes -> routes
-                        .requestMatchers(Constant.API_ENDPOINT_AUTH + "/**").permitAll()
-                        .requestMatchers(Constant.API_ENDPOINT_USERS + "/**").authenticated()
                         .requestMatchers(Constant.ENDPOINT_H2 + "/**").permitAll()
                         .requestMatchers(Constant.ENDPOINT_SWAGGER + "/**").permitAll()
                         .requestMatchers(Constant.ENDPOINT_SWAGGER_RESOURCES + "/**").permitAll()
+                        .requestMatchers(Constant.API_ENDPOINT_AUTH + "/**").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, Constant.API_ENDPOINT_USERS).hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, Constant.API_ENDPOINT_USERS + Constant.ENDPOINT_USERS_PAGINATE).hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, Constant.API_ENDPOINT_USERS + Constant.ENDPOINT_USERS_ID).hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, Constant.API_ENDPOINT_USERS + Constant.ENDPOINT_USERS_ID).hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, Constant.API_ENDPOINT_USERS + Constant.ENDPOINT_USERS_ID).hasAnyRole("ADMIN", "USER")
+
                         .anyRequest().denyAll()
                 );
         return http.build();
