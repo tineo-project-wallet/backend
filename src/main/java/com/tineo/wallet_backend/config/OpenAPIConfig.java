80368531e72ce2c59017aaddf0f6cc9fd5821fd1
tineojf <92743762+tineodev@users.noreply.github.com>
2025-01-10 14:03:58 -0500

    chore: set config to use jwt in swagger docs

--- src/main/java/com/tineo/wallet_backend/config/OpenAPIConfig.java
+++ src/main/java/com/tineo/wallet_backend/config/OpenAPIConfig.java
@@ -3,7 +3,11 @@ package com.tineo.wallet_backend.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -20,7 +24,18 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         url = "http://localhost:8080",
                         description = "Local server"
                 )
+        },
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
         }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT authentication",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class OpenAPIConfig {
 }
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,9 +1,8 @@
 -- Insertar datos en app_user
---INSERT INTO `app_user` (`name`, `username`, `password`, `role`) VALUES
---    ('Wallet Admin', 'admin', 'admin', 'ROLE_ADMIN'),
---    ('Tineo', 'tineo', 'password', 'ROLE_USER'),
---    ('Luis', 'luis', 'password', 'ROLE_USER'),
---    ('Juan', 'juan', 'password', 'ROLE_USER'),
---    ('Pedro', 'pedro', 'password', 'ROLE_USER'),
---    ('Maria', 'maria', 'password', 'ROLE_USER');
-select * from app_user;
+INSERT INTO `app_user` (`name`, `username`, `password`, `role`) VALUES
+    ('Wallet Admin', 'admin', 'admin', 'ROLE_ADMIN'),
+    ('Tineo', 'tineo', 'password', 'ROLE_USER'),
+    ('Luis', 'luis', 'password', 'ROLE_USER'),
+    ('Juan', 'juan', 'password', 'ROLE_USER'),
+    ('Pedro', 'pedro', 'password', 'ROLE_USER'),
+    ('Maria', 'maria', 'password', 'ROLE_USER');
