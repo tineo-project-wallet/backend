de727325fdcc54276c9ddc1a96492f5209670995
tineojf <92743762+tineodev@users.noreply.github.com>
2025-02-10 13:01:11 -0500

    feat: add card to schema & data sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,8 +1,17 @@
--- Insertar datos en app_user
-INSERT INTO `app_user` (`name`, `username`, `password`, `role`) VALUES
-    ('Wallet Admin', 'admin', 'adminpassword', 'ROLE_ADMIN'),
-    ('Tineo', 'tineo', 'password', 'ROLE_USER'),
-    ('Luis', 'luiss', 'password', 'ROLE_USER'),
-    ('Juan', 'juann', 'password', 'ROLE_USER'),
-    ('Pedro', 'pedro', 'password', 'ROLE_USER'),
-    ('Maria', 'maria', 'password', 'ROLE_USER');
+-- Table app_user
+INSERT INTO `app_user` (`user_id`,`name`, `username`, `password`, `role`) VALUES
+    (1, 'Wallet Admin', 'admin', 'adminpassword', 'ROLE_ADMIN'),
+    (2, 'Tineo', 'tineo', 'password', 'ROLE_USER'),
+    (3, 'Luis', 'luiss', 'password', 'ROLE_USER'),
+    (4, 'Juan', 'juann', 'password', 'ROLE_USER'),
+    (5, 'Pedro', 'pedro', 'password', 'ROLE_USER'),
+    (6, 'Maria', 'maria', 'password', 'ROLE_USER');
+
+-- Table card
+INSERT INTO card (alias, available_balance, user_id) VALUES
+    ('Card 1', 100.00, 1),
+    ('Card 2', 200.00, 1),
+    ('Card 3', 300.00, 3),
+    ('Card 4', 400.00, 4),
+    ('Card 5', 500.00, 5),
+    ('Card 6', 600.00, 6);
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,5 +1,5 @@
 -- -----------------------------------------------------
--- Table `mydb`.`app_user`
+-- Table `app_user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `app_user` (
   `user_id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -10,4 +10,22 @@ CREATE TABLE IF NOT EXISTS `app_user` (
   PRIMARY KEY (`user_id`)
 );
 
+-- -----------------------------------------------------
+-- Table `card`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `card` (
+  `card_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `alias` VARCHAR(45) NOT NULL,
+  `available_balance` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  `user_id` BIGINT NOT NULL,
+  PRIMARY KEY (`card_id`),
+  CONSTRAINT `fk_card_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `app_user` (`user_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+-- Indexes
 CREATE UNIQUE INDEX `username_UNIQUE` ON `app_user` (`username` ASC);
+CREATE INDEX `fk_card_user_idx` ON `card` (`user_id` ASC);
