29c70103543fb213b4b32bab6c1e389cf6bb169c
tineojf <92743762+tineodev@users.noreply.github.com>
2025-03-01 12:02:37 -0500

    fix: update repository implement

--- src/main/java/com/tineo/wallet_backend/repository/CardRepository.java
+++ src/main/java/com/tineo/wallet_backend/repository/CardRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CardRepository extends JpaRepository<Long, CardModel> {
+public interface CardRepository extends JpaRepository<CardModel, Long> {
 }
